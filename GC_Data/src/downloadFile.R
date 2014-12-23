763cff380542795bfd5297762689c659b14824ae
CjTouzi <cjtouzi@gmail.com>
2014-12-23 10:35:19 +0800

    download file src

---
+++ GC_Data/src/downloadFile.R
@@ -0,0 +1,21 @@
+# checking & creating directories 
+
+if (!file.exists("data")){
+
+    dir.create("data")
+}
+
+# download data from internet
+
+fileUrl="https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl,destfile = "./data/cameras.csv", method = "auto")
+
+
+list.files("./data")
+dateDownloaded <- date()
+dateDownloaded
+
+fileUrl1 <- "https://dl.dropboxusercontent.com/u/7710864/data/reviews-apr29.csv"
+fileUrl2 <- "https://dl.dropboxusercontent.com/u/7710864/data/solutions-apr29.csv"
+download.file(fileUrl1, destfile="./data/reviews.csv", method="curl")
+download.file(fileUrl2, destfile="./data/solutions.csv", method="curl")
