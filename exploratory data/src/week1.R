def27d276be6e1f91235169e2b30bde199b9ad0b
CjTouzi <cjtouzi@gmail.com>
2014-12-31 18:52:07 +0800

    week1-expdata

--- exploratory data/src/week1.R
+++ exploratory data/src/week1.R
@@ -14,6 +14,7 @@
 # principle 4: integration of evidence
  # - completedly integrate words, numbers, images, diagrams
  # - Data graphs
+
 # principle 5: Descibe and document the evident with appropriate labels,scales, sources
 
 # principle 6: Content is king
@@ -61,6 +62,71 @@ library(ggplot2)
 data(mpg)
 qplot(displ, hwy, data=mpg)
 
+library(datasets)
+hist(airquality$Ozone)
+with(airquality, plot(Wind,Ozone))
+airquality <- transform(airquality, Month = factor(Month))
+boxplot(Ozone ~ Month, airquality, xlab="Month", ylab="Ozone (ppb)")
+par("lty")
+with(airquality, plot(Wind, Ozone))
+title(main = "Ozone in New York")
+with(airquality, plot(Wind, Ozone, main="Ozone in New Yor"))
+with(subset(airquality, Month==5), points(Wind, Ozone, col="blue"))
+with(airquality, plot(Wind, Ozone, main="Ozone in New Yory"), type="n")
+with(subset(airquality, Month==5), points(Wind, Ozone, col="blue"))
+with(subset(airquality, Month!=5), points(Wind, Ozone, col="red"))
+legend("topright", pch=1, col=c("blue","red"),legend=c("May","Other Month"))
+
+with(airquality, plot(Wind, Ozone, main="Ozone in New Yor"), pch=20)
+model <- lm(Ozone ~Wind,airquality)
+abline(model, lwd=2)
+
+par(mfrow=c(1,2), oma=c(0,0,2,0))
+with(airquality, {
+    plot(Wind,Ozone, main="Ozone and Wind")
+    plot(Solar.R,Ozone, main="Ozone and Solar")
+    mtext("Weather", outer=TRUE)
+})
+
+x <- rnorm(100)
+hist(x)
+y <- rnorm(100)
+plot(x,y)
+par("mar")
+example(points)
+title("scatter plot")
+legend("topleft", legend = "Data", pch=1)
+fit <- lm(y~x)
+abline(fit, col="blue", lwd=3)
+plot(x, y, xlab="x", )
+
+x <- rnorm(100)
+y <- rnorm(100)
+g <- gl(2,50)
+g <- gl(2,50, labels =c("Male", "Female"))
+str(g)
+
+plot(x, y, type = "n")
+points(x[g=="Male"], y[g=="Male"], col="green")
+points(x[g=="Female"], y[g=="Female"], col="blue", pch=19)
+?devices
+
+# pdf 
+# svg
+# postcript
+
+# Bitmap
+# png
+# jpeg
+#tiff
+# bmp
 
 
 
+pdf(file ="myplot.pdf")
+with(faithful, plot(eruptions, waiting))
+title(main="Old")
+dev.off()
+
+dev.copy(png, file="gey.png")
+dev.off()
