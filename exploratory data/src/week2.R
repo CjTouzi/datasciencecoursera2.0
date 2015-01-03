35a4b41b899a3eca62d2700e59f60ea211f02b05
CjTouzi <cjtouzi@gmail.com>
2015-01-03 10:32:40 +0800

    week2

--- exploratory data/src/week2.R
+++ exploratory data/src/week2.R
@@ -40,14 +40,56 @@ qplot(displ, hwy, data=mpg,geom=c("point", "smooth"))
 
 qplot(hwy,data=mpg,fill=drv)
 qplot(displ, hwy, data=mpg, facets= .~drv)
+qplot(displ, hwy, data=mpg, facets= .~drv, geom = c("point","smooth"), method="lm")
 qplot(hwy,data=mpg,facets=drv~.,binwidth=2)
 
-str(maacs)
+
+
+# plot the data
+# overlay a summary
+# 
+
+g<- ggplot(mpg, aes(displ, hwy))
+
+g+ geom_point()+geom_smooth()
+
+g+ geom_point()+geom_smooth(method="lm")
 
 
 
+g+facet_grid(.~drv)+geom_point(color = "steelblue", size=4, alpha=0.5)+geom_smooth(size=2,linetype=1,method="lm")
+
+g+geom_point(aes(color =drv), size=4, alpha=0.5)+geom_smooth(size=2,linetype=1,method="lm")+ theme_bw(base_family = "Times")
 
 
+# outlier
+
+testdat <- data.frame(x=1:100, y=rnorm(100))
+testdat[50,2] <- 100 # outlier
+plot(testdat$x, testdat$y, type="l", ylim=c(-3,3))
+
+g <- ggplot(testdat, aes(x=x,y=y))
+g+ geom_line()+ ylim(-3,3)
+
+g+ geom_line()+ coord_cartesian(ylim=c(-3,3))
+
+
+
+# categorize the continuous variable 
+# calculate the deciles of the data
+str(mpg)
+
+cutpoint <- quantile(mpg$displ, seq(0,1,length=4),na.rm = TRUE)
+
+# cut the data at the deciles and crate a new factor varible
+
+mpg$displ2dec <- cut(mpg$displ, cutpoint)
+levels(mpg$displ2dec)
+
+g<- ggplot(mpg, aes(displ, hwy))
+g + geom_point() +facet_wrap(fl~class, nrow=7, ncol=5)+ geom_smooth(method="lm", se=FALSE, col="steelblue")+ theme_bw(base_family = "Avenir", base_size = 10)
+
+  
 
 
 
