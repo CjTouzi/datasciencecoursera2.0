da1a7c9f24c1d1e528c4dd542c52f244ea2d698a
CjTouzi <cjtouzi@gmail.com>
2015-01-03 10:32:35 +0800

    quiz2

---
+++ exploratory data/src/quiz2.R
@@ -0,0 +1,24 @@
+
+
+# Q2 ----------------------------------------------------------------------
+
+library(nlme)
+library(lattice)
+xyplot(weight ~ Time | Diet, BodyWeight)
+
+
+
+# Q7 ----------------------------------------------------------------------
+
+
+library(datasets)
+data(airquality)
+airquality = transform(airquality, Month = factor(Month))
+qplot(Wind, Ozone, data = airquality, facets = . ~ Month)
+
+
+# Q10 ---------------------------------------------------------------------
+
+
+library(ggplot2)
+qplot(votes, rating, data = movies) + geom_smooth()
