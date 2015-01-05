2c7053380a99179bf4326c11d839cbb97c53af4a
CjTouzi <cjtouzi@gmail.com>
2015-01-06 07:54:57 +0800

    week1 stat_infer

---
+++ week1.R
@@ -0,0 +1,16 @@
+
+# beta distribution 
+x <- c(-0.5, 0,1,1,1.5)
+y <- c(0,0,2,0,0)
+plot(x, y, lwd=3, frame=FALSE, type="l")
+1.5*0.75/2
+
+pbeta(0.75,2,1)
+
+pbeta(c(0.4,0.5,0.6),2,1)
+
+# qantile 
+
+qbeta(0.5,2,1)
+
+
