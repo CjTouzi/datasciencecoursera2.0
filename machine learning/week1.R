534a1bd0ef4eb6f761c5bcd857f54e7d189be9a2
CjTouzi <cjtouzi@gmail.com>
2015-01-23 12:29:37 +0800

    ml quiz1

--- machine learning/week1.R
+++ machine learning/week1.R
@@ -10,6 +10,39 @@ prediction <- ifelse(spam$your>0.5,"spam","nonspam")
 table(prediction, spam$type)/length(spam$type)
 
 # accuracy ~ 0.4590306+ 0.2923278
+library(kernlab)
+data(spam);set.seed(333)
+smallSpam <- spam[sample(dim(spam)[1],size=10),]
+spamLabel <- (smallSpam$type=="spam")*1+1
+plot(smallSpam$capitalAve,col=spamLabel)
+
+# prediction rule 
+
+rule1 <- function(x) {
+        
+        prediction <- rep(NA,length(x))
+        prediction[x>2.7] <- "spam"
+        prediction[x<2.4] <- "nonspam"
+        prediction[(x >= 2.4 & x <=2.45)] <- "spam"
+        prediction[(x>2.45 & x<=2.7)] <- "nonspam"
+        return(prediction)
+}
+table(rule1(smallSpam$capitalAve), smallSpam$type)
 
 
+rule2 <- function(x) {
+        
+        prediction <- rep(NA, length(x))
+        prediction[x>2.8] <- "spam"
+        prediction[x<=2.8] <- "nonspam"
+        return(prediction)
+}
 
+
+table(rule2(smallSpam$capitalAve), smallSpam$type)
+
+
+table(rule1(spam$capitalAve), spam$type)
+table(rule2(spam$capitalAve), spam$type)
+sum(table(rule1(spam$capitalAve)==spam$type))
+sum(table(rule2(spam$capitalAve)==spam$type))
