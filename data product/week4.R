be61e821a70f5984c021c73b43ee255cd1d8dbcf
CjTouzi <cjtouzi@gmail.com>
2015-02-04 22:11:41 +0800

    week4 lec R package & class and methods

---
+++ data product/week4.R
@@ -0,0 +1,15 @@
+library(methods)
+setClass("polygon", representation(x="numeric", y="numeric"))
+
+setMethod("plot", "polygon",
+          function(x, y, ...) {
+              plot(x@x, x@y, type = "n", ...)
+              xp <- c(x@x, x@x[1])
+              yp <- c(x@y, x@y[1])
+              lines(xp, yp)
+          })
+
+library(methods)
+showMethods("plot")
+p <- new("polygon", x = c(1, 2, 3, 4), y = c(1, 2, 3, 1))
+plot(p)
