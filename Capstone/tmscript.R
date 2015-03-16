6efe22eb592dc56a134f6500b4ac43d98e7017fe
CjTouzi <cjtouzi@gmail.com>
2015-03-16 16:13:54 +0800

    update

--- Capstone/tmscript.R
+++ Capstone/tmscript.R
@@ -1,7 +1,7 @@
 
 # init --------------------------------------------------------------------
 
-libs <-c("tm", "dplyr","NLP","openNLP")
+libs <-c("tm", "dplyr","NLP","openNLP","RWeka")
 lapply(libs, require, character.only=TRUE)
 # set options
 options(stringsAsFactors = FALSE)
@@ -17,19 +17,24 @@ dir <- "./data/en_US/"
 
 # clean text --------------------------------------------------------------
 
+TestText <- "Text mining, 
+also referred to as text data mining, 
+roughly equivalent to text analytics, 
+refers to the process of deriving high-quality information from text. 
+High-quality information is typically derived through the devising of patterns 
+and trends through means such as statistical pattern learning."
 
-con <- readLines("./data/en_US/en_US.blogs.txt",50, encoding="UTF-8")
-con <- strsplit(con, "[,;!?|\n\\.]^[0-9]")
+# The testText has two sentences is divided by comma 
+# demonstrate the unwanted tokens using test corpus: sentence and phrase junction
 
-head(con)
 
-# tokenization: 
-# the purpose of this project is to predict the following world given a sequence of words.
-# These words are surposed to form a a sentences
-# Therefore, we use the period and comma as my 
+#  the unwanted term "text High-quality information" caused by sentence junction
+# "mining also referred",  "text analytics refers" and so on
 
+# to remove unwanted tokens by sentence junction, we using sentence detector provided by openNLP
 
 convert_text_to_sentences <- function(text, lang = "en") {
+    
     # Function to compute sentence annotations using the Apache OpenNLP Maxent sentence detector employing the default model for language 'en'. 
     sentence_token_annotator <- Maxent_Sent_Token_Annotator(language = lang)
     
@@ -46,50 +51,66 @@ convert_text_to_sentences <- function(text, lang = "en") {
     return(sentences)
 }
 
-corpus <- convert_text_to_sentences(con, lang="en")
-head(corpus)
+# then we further seperate the sentences by comma 
 
-tokenCorpus <- function(file){
+convert_text_to_sentences_fragment_corpus <- function(text,lang="en"){
     
-    con <- readLines(file,50)
-    corpus.tmp <- strsplit(con, " ")
-    
+    # docs<- readLines(file,nline, encoding="UTF-8")
+    text <- {
+        convert_text_to_sentences(text,lang) %>%
+        strsplit("[,\n]") %>%        
+        unlist()
+    }
+
 }
 
 
-
-
-# clean text
-
-cleanCorpus <- function(corpus){
+convert2Corpus <- function(text){
     
     corpus.tmp <- {
-        corpus %>%
-            tm_map(removePunctuation) %>%
-            tm_map(stripWhitespace) %>%
-            tm_map(tolower) %>%
-            tmp(removeWords, stopwords("english"))
+            text %>%
+            unlist() %>%
+            as.vector() %>%
+            VectorSource() %>%
+            Corpus() 
     }
- 
+    
 }
-
-
-# build TDM
-
-
 
 
 
 # build TDM ---------------------------------------------------------------
 
 
-# attach name -------------------------------------------------------------
+
+tdm <- TermDocumentMatrix(TestCorpus,
+                          control = list(removePunctuation = TRUE,
+                                         stopwords = TRUE, 
+                                         stripWhitespace=TRUE,
+                                         tolower=TRUE))
+tdm[1:2,1:5]
+
+TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
 
 
-# stack -------------------------------------------------------------------
+tdm <- TermDocumentMatrix(TestCorpus,
+                          control = list(removePunctuation = TRUE,
+                                         stopwords = TRUE, 
+                                         stripWhitespace=TRUE,
+                                         tolower=TRUE, 
+                                         tokenize = TrigramTokenizer))
+inspect(tdm[1:2,1:5])
+
+
+
+con <- readLines("./Coursera-SwiftKey/final/en_US/en_US.blogs.txt",50, encoding="UTF-8")
+
+head(con)
 
 
 # hold-out ----------------------------------------------------------------
+
+
 
 
 # model -------------------------------------------------------------------
