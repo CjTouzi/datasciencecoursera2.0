7d902d6aac4721bd111b77e8273be737000a918e
CjTouzi <cjtouzi@gmail.com>
2015-03-17 17:23:00 +0800

    update

--- Capstone/tmscript.R
+++ Capstone/tmscript.R
@@ -1,7 +1,7 @@
 
 # init --------------------------------------------------------------------
 
-libs <-c("tm", "dplyr","NLP","openNLP","RWeka")
+libs <-c("tm", "dplyr","NLP","openNLP","RWeka","SnowballC")
 lapply(libs, require, character.only=TRUE)
 # set options
 options(stringsAsFactors = FALSE)
@@ -12,17 +12,12 @@ Sys.setenv(LANG = "en_US.UTF-8")
 
 # set parameters
 
-dir <- "./data/en_US/"
+foldPath<- "./Coursera-SwiftKey/final//en_US/"
+files <- paste0(foldPath,list.files(foldPath))
+files
 
 
 # clean text --------------------------------------------------------------
-
-TestText <- "Text mining, 
-also referred to as text data mining, 
-roughly equivalent to text analytics, 
-refers to the process of deriving high-quality information from text. 
-High-quality information is typically derived through the devising of patterns 
-and trends through means such as statistical pattern learning."
 
 # The testText has two sentences is divided by comma 
 # demonstrate the unwanted tokens using test corpus: sentence and phrase junction
@@ -65,7 +60,8 @@ convert_text_to_sentences_fragment_corpus <- function(text,lang="en"){
 }
 
 
-convert2Corpus <- function(text){
+
+convert_text_to_Corpus <- function(text){
     
     corpus.tmp <- {
             text %>%
@@ -78,34 +74,72 @@ convert2Corpus <- function(text){
 }
 
 
+# sampling process 
+# Given the large amount of text and limited computational resources, sampling is performed. 
+# 10000 sentences fragments are randomly sampled, combined to corpus and finally saved to disk.
+
+
+# sn <- 10000
+# mytext <- ""
+# for (f in files){
+#     print(f)
+#     text.tmp <- readLines(f,sn) 
+#     text.tmp <- convert_text_to_sentences_fragment_corpus(text.tmp,"en")
+# remove non-ASCII characters
+# link: http://stackoverflow.com/questions/18153504/removing-non-english-text-from-corpus-in-r-using-tm
+
+
+#     text.tmp <- text.tmp[sample(1:length(text.tmp),sn)]
+#     mytext <- c(text.tmp,mytext)
+#     print(mytext)
+# }
+
+# clean Corpus
+
+load("mytext.RData")
+myCorpus <- convert_text_to_Corpus(mytext)
+myCorpus <- {
+    
+    # convert to lowercase
+     tm_map(myCorpus, content_transformer(tolower)) %>%
+                tm_map(removePunctuation) %>%
+                tm_map(removeNumbers) %>%
+                tm_map(stripWhitespace) %>%
+                tm_map(removeWords, stopwords("english")) 
+    
+}
+save(myCorpus, file="myCorpus.RData")
+# save(mytext,file="mytext.RData")
+inspect(myCorpus)
+
+
 
 # build TDM ---------------------------------------------------------------
 
 
+getNgramTDM <- function(corpus, ngram){
+    
+    Tokenizer <- function(x) NGramTokenizer(x, Weka_control(min = ngram, max = ngram))
+   
+    tdm <-  TermDocumentMatrix(corpus,
+                              control = list(tokenize = Tokenizer))
+            
+}
+tdm1 <- getNgramTDM(myCorpus,1)
 
-tdm <- TermDocumentMatrix(TestCorpus,
-                          control = list(removePunctuation = TRUE,
-                                         stopwords = TRUE, 
-                                         stripWhitespace=TRUE,
-                                         tolower=TRUE))
-tdm[1:2,1:5]
-
-TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
-
-
-tdm <- TermDocumentMatrix(TestCorpus,
-                          control = list(removePunctuation = TRUE,
-                                         stopwords = TRUE, 
-                                         stripWhitespace=TRUE,
-                                         tolower=TRUE, 
-                                         tokenize = TrigramTokenizer))
-inspect(tdm[1:2,1:5])
+load("myCorpus.RData")
+myCorpus[[98]]
 
 
+# Exploratory analysis ----------------------------------------------------
 
-con <- readLines("./Coursera-SwiftKey/final/en_US/en_US.blogs.txt",50, encoding="UTF-8")
+# find Frequency Per Term
+# http://stackoverflow.com/questions/14426925/frequency-per-term-r-tm-documenttermmatrix
+num <- 10 # Show this many top frequent terms
+tdm1[findFreqTerms(tdm1,100)[1:num],]%>%
+    as.matrix() %>%
+    rowSums()
 
-head(con)
 
 
 # hold-out ----------------------------------------------------------------
