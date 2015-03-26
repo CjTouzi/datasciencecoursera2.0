58ea184c446a66a80b3c331e629fb17bd33c2664
CjTouzi <cjtouzi@gmail.com>
2015-03-26 10:45:21 +0800

    update: report and script.
    To do: the backoff model haven't finish

--- Capstone/tmscript.R
+++ Capstone/tmscript.R
@@ -1,7 +1,7 @@
 
 # init --------------------------------------------------------------------
 
-libs <-c("tm", "dplyr","NLP","openNLP","RWeka","SnowballC","wordcloud")
+libs <-c("tm", "dplyr","NLP","openNLP","RWeka","SnowballC","wordcloud","ggplot2", "slam")
 lapply(libs, require, character.only=TRUE)
 # set options
 options(stringsAsFactors = FALSE)
@@ -229,21 +229,27 @@ save(tdm4,file="tdm4.RData")
 # http://stackoverflow.com/questions/14426925/frequency-per-term-r-tm-documenttermmatrix
 rm(myTrain)
 
-
+load("tdm1.RData")
+load("tdm2.RData")
+load("tdm3.RData")
 
 # remove none frequent terms
 # Show this many top frequent terms
 
 
 ## 1gram
+
 length(findFreqTerms(tdm1,200))
 
-tdm1.topFreq<- tdm1[findFreqTerms(tdm1,200),]%>%
+tdm1.topFreq<- tdm1[findFreqTerms(tdm1,200)]%>%
     as.matrix() %>%
     rowSums() 
-
-head(tdm1.topFreq)
 tdm1.topFreq <- sort(tdm1.topFreq, decreasing=TRUE)
+
+x <- barplot(table(mtcars$cyl), xaxt="n")
+labs <- paste(names(table(mtcars$cyl)), "cylinders")
+text(cex=1, x=x-.25, y=-1.25, labs, xpd=TRUE, srt=45, pos=2)
+
 
 ## 2gram
 length(findFreqTerms(tdm2,20))
@@ -289,6 +295,108 @@ wordcloud(names(tdm2.topFreq), tdm2.topFreq, max.words=50, scale=c(5, .1), color
 wordcloud(names(tdm3.topFreq), tdm3.topFreq, max.words=50, scale=c(5, .1), colors=brewer.pal(6, "Dark2"))
 
 
-# model -------------------------------------------------------------------
+
+
+## train N-gram models---------------------------------------
+## term-document matrix combined unigram, bigram, trigram
+
+load("myTrain.RData")
+Tokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 3))
+tdm <-  TermDocumentMatrix(myTrain,
+                           control = list(tokenize = Tokenizer))
+
+
+
+save(tdm,file="tdm.RData")
+
+## convert term document matrix into condictional probability
+load("tdm.RData")
+
+
+# unigram probword1)= count(word1)/total number of word
+# bigram prob(word2|word1)= count(word1 word2)/count(word1)
+# trigram prob(word3|word1 word2)= count(word1 word2 word3)/count(word1 word2)
+convertTDM_to_Freq_DF <- function(tdm1){
+    
+    tdm1.Freq <- rollup(tdm1, 2, na.rm=TRUE, FUN = sum)
+    tdm1.Freq.mat<- as.matrix(tdm1.Freq[findFreqTerms(tdm1.Freq,1),])
+    term <- row.names(tdm1.Freq.mat)
+    tdm1.Freq.df <- data.frame(term=term, freq= tdm1.Freq.mat[1:nrow(tdm1.Freq.mat)])
+    
+    
+}
+
+tdm.Freq.df <- convertTDM_to_Freq_DF(tdm)
+
+get_first_n1_word(head(tdm.Freq.df$term)[[3]])
+term <- strsplit(tdm.Freq.df$term," ")
+
+term.length <- unlist(lapply(term, function(i) length(term[i])))
+tdm.Freq.df <- cbind(tdm.Freq.df, term.length)
+rm(term)
+
+
+unigram.log.cond.prob <- log(tdm.Freq.df[tdm.Freq.df$term.length==1,]$freq)-log(sum(tdm.Freq.df[tdm.Freq.df$term.length==1,]$freq,na.rm=TRUE))
+head(unigram.log.cond.prob)
+    
+
+#root words are the words first n-1 words in a n word term
+
+get_root_word <- function(str, sep=" "){
+    
+    strl <- unlist(strsplit(str,sep))
+    paste(strl[1:length(strl)-1], collapse = sep)
+    
+    
+}
+
+root.words <- apply(as.data.frame(tdm.Freq.df$term), 1, function(x) get_root_word(x))
+tdm.Freq.df$root.words <- root.words
+
+
+
+# count the root words frequence 
+root.words.freq <- data.frame(root.words.freq=rep(0,nrow(tdm.Freq.df)))
+
+# case 2
+
+for (leaf.index in 1:nrow(tdm.Freq.df)){
+
+    if (tdm.Freq.df$term.length[[leaf.index]]==2){
+        root.freq=tdm.Freq.df$freq[[leaf.index]]
+        next  
+    }
+    if (tdm.Freq.df$term.length[[leaf.index]]==3){
+        root.words.freq$root.words.freq[[leaf.index]] <- root.freq
+    }
+  
+    
+}
+
+save(root.words.freq,file="rootwordsfreq.RData")
+
+# case 1
+
+for (leaf.index in 1:nrow(tdm.Freq.df)){
+    
+    if (tdm.Freq.df$term.length[[leaf.index]]==1){
+        root.freq=tdm.Freq.df$freq[[leaf.index]]
+        next  
+    }
+    if (tdm.Freq.df$term.length[[leaf.index]]==2){
+        root.words.freq$root.words.freq[[leaf.index]] <- root.freq
+    }
+    
+    
+}
+
+
+tdm.Freq.df$root.words.freq <- root.words.freq$root.words.freq
+
+save(tdm.Freq.df,file="tdm.Freq.df.RData")
+
+
+
+
 
 
