a6a9f407f49d0a120a8b951f9483968bbd559b95
CjTouzi <cjtouzi@gmail.com>
2015-04-20 17:59:05 +0800

    final prediction model

--- Capstone/tmscript.R
+++ Capstone/tmscript.R
@@ -316,8 +316,10 @@ load("tdm.RData")
 # unigram probword1)= count(word1)/total number of word
 # bigram prob(word2|word1)= count(word1 word2)/count(word1)
 # trigram prob(word3|word1 word2)= count(word1 word2 word3)/count(word1 word2)
+
+
 convertTDM_to_Freq_DF <- function(tdm1){
-    
+    require(slam)
     tdm1.Freq <- rollup(tdm1, 2, na.rm=TRUE, FUN = sum)
     tdm1.Freq.mat<- as.matrix(tdm1.Freq[findFreqTerms(tdm1.Freq,1),])
     term <- row.names(tdm1.Freq.mat)
@@ -394,6 +396,114 @@ for (leaf.index in 1:nrow(tdm.Freq.df)){
 tdm.Freq.df$root.words.freq <- root.words.freq$root.words.freq
 
 save(tdm.Freq.df,file="tdm.Freq.df.RData")
+
+# simple stupid backoff model
+
+# 1. measure the length of the input setence
+# 2. 1) length>=2:trigram backoff;2) length=1 bigram  
+
+
+
+## back_off_model
+library(dplyr)
+load("tdm.Freq.df.RData")
+input.setence <- "If it returns"
+back_off_model(input.setence,tdm.Freq.df)
+
+back_off_model <- function(input.setence,tdm.Freq.df){
+    
+# trigram prediction
+    # get the last two words
+    root <- get_rootword(input.setence,2)
+    re <- predict_next_word(root,tdm.Freq.df)
+    re
+    if (nrow(re)==0){
+        # bigram
+        root <- get_rootword(input.setence,1)
+        re <- predict_next_word(root,tdm.Freq.df)
+        
+        if(nrow(re)==0){
+            # unigram
+            re <- {
+                tdm.Freq.df %>% 
+                    filter(term.length==1) %>%
+                    filter(freq==max(freq))
+            }
+            return(get_rootword(re$term,1))
+
+        }
+        
+        # bigram finished
+        if (nrow(re)==1){
+            return(get_rootword(re$term,1))
+        }
+        
+        if (nrow(re)>1){
+            # use the most frequent
+            
+            pred.phrase <-unlist(lapply(re$term, get_rootword, lastn=1))
+            temp <- tdm.Freq.df[tdm.Freq.df$term %in% pred.phrase,]
+            re <- temp[which.max(temp$freq),]
+            return(get_rootword(re$term,1))
+        }
+        
+    }
+    
+    if (nrow(re)==1){
+        return(get_rootword(re$term,1))
+    }
+    
+    # return multiple results, find the most frequent term
+    if (nrow(re)>1){
+        
+        pred.phrase <-unlist(lapply(re$term, get_rootword, lastn=2))
+        temp <- tdm.Freq.df[tdm.Freq.df$term %in% pred.phrase,]
+        re <- temp[which.max(temp$freq),]
+        
+        return(get_rootword(re$term,1))
+    }
+
+}
+
+predict_next_word <- function(root, tdm.Freq){
+    
+    re <- {
+        tdm.Freq%>%
+            filter(root.words==root) %>% 
+            filter(freq==max(freq))
+        
+    }
+    re
+  
+}
+
+# get_freq_term <- function(my_term, tdm.Freq){
+#     
+#     re <- {
+#         tdm.Freq%>%
+#             filter(term==my_term) %>% 
+#             filter(freq==max(freq))
+#         
+#     }
+#     re
+#     
+#     
+# }
+
+
+get_rootword <- function(input.setence, lastn){
+    
+    words <- unlist(strsplit(input.setence, split=" "))
+    n.word <- length(words)
+    
+    if(n.word<=lastn){
+        return(input.setence)
+        
+    }
+   
+    paste(words[(n.word-lastn+1):n.word],collapse =" ")
+    
+}
 
 
 
